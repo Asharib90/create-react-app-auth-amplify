bbd9dcc0c9e940a7351df133a88965e87eff70ff
arsalanusmanstar <92919937+arsalanusmanstar@users.noreply.github.com>
2021-12-28 19:49:40 +0500

    updated

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,28 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "cognitocf0c6096": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "AppClientSecret": "string"
+        }
+    },
+    "function": {
+        "createreactappautham569336e0": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "api8b2dbebb": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
