adbf6fa625396b0a003ac3cbc783c4155972c9b3
Asharib Ahmed <asharibahmed143@gmail.com>
2021-12-29 11:55:59 +0500

    updated

--- amplify/backend/function/createreactappautham569336e0/src/app.js
+++ amplify/backend/function/createreactappautham569336e0/src/app.js
@@ -6,22 +6,69 @@ or in the "license" file accompanying this file. This file is distributed on an 
 See the License for the specific language governing permissions and limitations under the License.
 */
 
-
-
-
 var express = require('express')
 var bodyParser = require('body-parser')
 var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
 
+const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient
+const { getForm, addOrUpdateForm} = require('./dynamo');
+//insert post
+
+const { postInsert } = require('./controllers/post/postInsert');
+
+//delete post
+const {postDelete} = require('./controllers/post/postDelete');
+
+
+//get singe post
+
+const {postSingleGet} = require('./controllers/post/postSingleGet');
+
+//update post
+
+const {postUpdate} = require('./controllers/post/postUpdate');
+
 // declare a new express app
 var app = express()
+app.use(cors());
+
 app.use(bodyParser.json())
 app.use(awsServerlessExpressMiddleware.eventContext())
+
+const connection = MongoClient.connect('mongodb+srv://starmarketing:Crystalball007@cluster0.u64nt.mongodb.net',
+ { useUnifiedTopology: true })
+
+
+var i=1
+app.post('/form',async(req, res) => {
+    try {
+        // console.log(req.body)
+        const data = await addOrUpdateForm({...req.body,id:i+''})
+        i++
+        res.json(data)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({err:"Something went wrong"})
+    }
+})
+
+app.get('/form',async(req, res) => {
+    try {
+        const data = await getForm()
+        res.json(data)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({err:"Something went wrong"})
+    }
+})
 
 // Enable CORS for all methods
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", "*")
   res.header("Access-Control-Allow-Headers", "*")
+  res.header("Access-Control-Allow-Credentials", true)
   next()
 });
 
@@ -30,38 +77,65 @@ app.use(function(req, res, next) {
  * Example get method *
  **********************/
 
-app.get('/post', function(req, res) {
+ app.get('/post', function(req, res) {
   // Add your code here
   res.json({success: 'get call succeed!', url: req.url});
+});
+app.get('/', function(req, res) {
+  // Add your code here
+  res.json({success: 'get call succeed!'});
 });
 
-app.get('/post/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'get call succeed!', url: req.url});
-});
+// app.get('/post/*', function(req, res) {
+//   // Add your code here
+//   res.json({success: 'get call succeed!', url: req.url});
+// });
+
+app.get('/post/:id',postSingleGet(connection))
+
+//getSinglePost
+// app.get('/post/:id',function(req,rest){
+//   var query = { '_id' : req.params.id };
+//   console.log(query);
+//   console.log('query');
+// connection.then(client => {    
+//   const post = client.db('news').collection('post').findOne(query).then(function(results){
+   
+//       // res.send(results.deletedCount? "Deleted" :"record not found");
+//       if(!results)
+//       throw new Error('No record found.');
+//       console.log(results);//else case
+    
+   
+//   })
+//   .catch(error => console.error(error))
+//   })
+// })
+
 
 /****************************
 * Example post method *
 ****************************/
 
-app.post('/post', function(req, res) {
-  // Add your code here
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
-});
+app.post('/post',postInsert(connection))
 
 app.post('/post/*', function(req, res) {
   // Add your code here
   res.json({success: 'post call succeed!', url: req.url, body: req.body})
 });
 
+
+
 /****************************
 * Example put method *
 ****************************/
 
-app.put('/post', function(req, res) {
-  // Add your code here
-  res.json({success: 'put call succeed!', url: req.url, body: req.body})
-});
+// app.put('/post', function(req, res) {
+//   // Add your code here
+//   res.json({success: 'put call succeed!', url: req.url, body: req.body})
+// });
+
+app.put('/post',postUpdate(connection))
 
 app.put('/post/*', function(req, res) {
   // Add your code here
@@ -72,17 +146,14 @@ app.put('/post/*', function(req, res) {
 * Example delete method *
 ****************************/
 
-app.delete('/post', function(req, res) {
-  // Add your code here
-  res.json({success: 'delete call succeed!', url: req.url});
-});
+app.delete('/post' ,postDelete(connection))
 
 app.delete('/post/*', function(req, res) {
   // Add your code here
   res.json({success: 'delete call succeed!', url: req.url});
 });
 
-app.listen(3000, function() {
+app.listen(3001, function() {
     console.log("App started")
 });
 
---
+++ amplify/backend/function/createreactappautham569336e0/src/controllers/post/postInsert.js
@@ -0,0 +1,100 @@
+const { response } = require("express");
+const fetch = require ("node-fetch");
+
+const filter = {}
+const  postInsert=(connection)=>(req,res)=>{ 
+    getValueForNextSequence(connection,"item_id")
+    const {title,slug,link,description,excerpt,featuredImage,featured,date,category,author,tags, seo}=req.body
+    const newDate = new Date();
+    
+    
+
+    title? filter['title']=title : 0
+    slug? filter['slug']=slug : filter['slug']='/'+title.toLowerCase().split(' ').join('-')
+    link? filter['link']=null : filter['link']=null
+    description? filter['description']=description : 0
+    excerpt? filter['excerpt']=excerpt : 0
+    featuredImage? filter['featuredImage']=featuredImage : 0
+    featured? filter['featured']=featured : filter['featured']=featured
+    date? 0: filter['date'] = newDate.getFullYear()+'-'+(newDate.getMonth()+1)+'-'+newDate.getDate() 
+    category? filter['category']=category : 0
+    author? filter['author']=author : 0
+    tags? filter['tags']=tags : filter['tags']=tags
+    
+     
+    seo?filter['seo']=seo:0
+    
+    
+    
+
+    connection.then(client => {
+    const post = client.db('news').collection('post')  
+
+    const quotesCollection = post.insertOne(filter)
+
+   
+    .then(results => {
+    try {
+      if(req.body.category==1)
+      {
+        
+        fetch('https://webhooks.amplify.us-east-2.amazonaws.com/prod/webhooks', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            id: '052d5733-d21f-4a42-8bcd-21261c279e04',
+            token: '3UAXRi5Kfw1Dky3Q1dgBAkRCV6JtNiXM56X8emphtbE',
+          })
+          
+        }).then(response => {
+          console.log(response)
+      })
+      }
+      else
+      {
+        
+        fetch('https://webhooks.amplify.us-east-2.amazonaws.com/prod/webhooks', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            id: 'fcdd9f38-ab8c-407c-814e-1d6ddd132d79',
+            token: '2aSSHXukua8PZG1nO6Z8ZgQkDcZy0tZesoaWP8011A',
+          })
+          
+        }).then(response => {
+          console.log(response)
+      })
+      }
+         
+        
+        res.send(results);
+      } catch (error) {
+        res.status(500).send(error);
+      }
+    })
+    .catch(error => console.error(error))
+    })
+}
+
+function getValueForNextSequence(connection,sequenceOfName){
+
+  connection.then(client => {
+    const post = client.db('news').collection('postIdSequence')
+    post.findOneAndUpdate(
+    { "_id": sequenceOfName },     
+    {"$inc":{sequence_value:1}})
+    .then(results => {      
+      filter['_id']=results.value.sequence_value+1
+    });    
+    })    
+ }
+
+module.exports={
+  postInsert:postInsert
+}
